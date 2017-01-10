a4163b2fb48b7e44f6b25da0309eb6a1166747da
Mike Christensen <mschristensen@users.noreply.github.com>
2017-01-10 11:08:21 +0000

    essential fix: fetch correct user from db using parsed token user._id

--- config/initialisers/passport.js
+++ config/initialisers/passport.js
@@ -7,7 +7,7 @@ module.exports = function(passport) {
   opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
   opts.secretOrKey = process.env.SECRET;
   passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    User.findOne({ id: jwt_payload.id }, function(err, user) {
+    User.findById(jwt_payload._doc._id, function(err, user) {
       if (err) {
         return done(err, false);
       }
